1c966d9e7f323db67d8a4de7330875e8432281e1
Neosk <neosekaleli@gmail.com>
2025-06-13 01:53:11 +0200

    UI displayed

--- src/app/(auth)/mail/page.tsx
+++ src/app/(auth)/mail/page.tsx
@@ -1,15 +1,26 @@
-import { UserButton } from "@clerk/nextjs"
-import { cookies } from "next/headers"
+import { Mail } from "@/app/mail/components/mail";
+import { UserButton } from "@clerk/nextjs";
+import { cookies } from "next/headers";
 
 export default function MailPage() {
-  const layout = cookies().get("react-resizable-panels:layout")
+  const layout = cookies().get("react-resizable-panels:layout:mail");
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
   return (
     <>
       <div className="md:hidden">
         <UserButton />
       </div>
       <div className="hidden flex-col md:flex">
+        <Mail
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
       </div>
     </>
-  )
+  );
 }
